f4196ef519ade45682581402d173bc84ad3ea22e
emagdnimMQT <mark.w.novak@gmail.com>
2020-06-23 23:32:13 -0400

    first commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">Randi Clemens</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default App;
