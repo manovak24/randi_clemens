e16658c6ccb2182530159dc11e89d7dfea59c105
emagdnimMQT <mark.w.novak@gmail.com>
2020-06-25 11:53:39 -0400

    udpate Footer to component

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,18 +2,21 @@ import React from 'react';
 import Container from 'react-bootstrap/container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { render } from '@testing-library/react';
 
-function Footer() {
-    return(
-        <footer className="mt-5">
-            <Container fluid={true}>
-                <Row className="border-top justify-content-between p-3">
-                    <Col className="p-0" md={3} sm={12}>Randi Clemens</Col>
-                    <Col className="p-0 d-flex justify-content-end" md={3}>This site was made by Mark Novak</Col>
-                </Row>
-            </Container>
-        </footer>
-    );
+class Footer extends React.Component {
+    render() {
+        return(
+            <footer className="mt-5">
+                <Container fluid={true}>
+                    <Row className="border-top justify-content-between p-3">
+                        <Col className="p-0" md={3} sm={12}>Randi Clemens</Col>
+                        <Col className="p-0 d-flex justify-content-end" md={3}>This site was made by Mark Novak</Col>
+                    </Row>
+                </Container>
+            </footer>
+        );
+    }
 }
 
 export default Footer;
