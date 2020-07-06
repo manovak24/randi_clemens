de6faf8e68cdd2f9cc995e0a6fdc49c3bdc03fcf
emagdnimMQT <mark.w.novak@gmail.com>
2020-07-06 09:52:00 -0400

    created content.js and updating aboutpage.js

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import Hero from '../Components/Hero/Hero';
+import Content from '../Components/Content/Content';
 
 function AboutPage(props) {
     return(
-        <p>AboutPage is under construction</p>
+        <div>
+            <Hero title={props.title} />
+            <Content>
+                <p>Add about info here</p>
+                <p>More info</p>
+                <p>Even more info</p>
+            </Content>
+        </div>
     )
 }
 
