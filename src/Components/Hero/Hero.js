b1c1df68e5ecee4c8b2959c50e7e0302c4e864f2
emagdnimMQT <mark.w.novak@gmail.com>
2020-07-14 08:26:47 -0400

    testing out background image for jumbotron

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -3,12 +3,13 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import './Hero.css';
 
 
 function Hero(props) {
     return (
         <Jumbotron className="bg-transparent jumbotron-fluid pd-o mb-0">
-            <Container fluid="true">
+            <Container className="jumboContainer" fluid="true">
                 <Row className="justify-content-center">
                     <Col md={8} sm={12}>
                         { props.title && <h1 className="display-1 font-weight-bolder border-bottom">{props.title}</h1> }
