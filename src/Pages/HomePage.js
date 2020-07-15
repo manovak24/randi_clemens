eea940dfab3365a9bc9e185f25da6bccff8d6699
emagdnimMQT <mark.w.novak@gmail.com>
2020-07-15 17:04:39 -0400

    changing layout of home page

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Hero from '../Components/Hero/Hero';
 import Carousel from '../Components/Cards/Carousel';
+import './HomePage.css'
 
 function HomePage(props) {
     return(
-        <div>
+        <div className="homePage">
             <Hero title={props.title} subTitle={props.subTitle} text={props.text} />
-            <Carousel />
         </div>
     );
 }
