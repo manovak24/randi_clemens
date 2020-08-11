9102196f951fec308520a29eb722383d38bc9285
emagdnimMQT <mark.w.novak@gmail.com>
2020-08-11 09:51:59 -0400

    updating contact page and add contact ppage css file

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Hero from '../Components/Hero/Hero';
+import { FaFemale } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
+
+import './ContactPage.css';
 
 function ContactPage(props) {
     return (
         <div>
-            <Hero title={props.title} />
+            <div>
+                <Hero title={props.title} />
+            </div>
+            <div className="contact-container">
+                <ul>
+                    <li><FaFemale /> Randi Clemens</li>
+                </ul>
+                <ul>
+                    <li><MdEmail /> Randi Clemens</li>
+                </ul>
+            </div>
         </div>
     )
 }
 
 export default ContactPage;
+
