b74daf8ecedaf9797264a4a585a6ea4404bb9d20
emagdnimMQT <mark.w.novak@gmail.com>
2020-08-15 14:08:30 -0400

    adding example contact info and social follow icons

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import emailjs from 'emailjs-com';
-import Swal from 'sweetalert2';
 
 import Hero from '../Components/Hero/Hero';
 import Content from '../Components/Content/Content';
+import SocialFollow from '../Components/SocialFollow/SocialFollow';
 
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import emailjs from 'emailjs-com';
+import Swal from 'sweetalert2';
+import { FaFemale } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
+
 
 class ContactPage extends React.Component {
     constructor(props) {
@@ -57,6 +61,11 @@ class ContactPage extends React.Component {
             <div>
                 <Hero title={this.props.title} />
                 <Content>
+                    <h6>Please feel free to reach out to me at one of my social media accounts listed below or fill out the contact page and I will get back to you as soon as possible. Thanks!</h6>
+                    <h4><FaFemale /> Randi Clemens</h4>
+                    <h4><MdEmail /> randi.clemens@gmail.com</h4>
+                    <SocialFollow />
+                    <br></br>
                     <Form onSubmit={this.handleSubmit}>
                         <Form.Group>
                             <Form.Label htmlFor="full-name">Full Name</Form.Label>
