ebe6ffa537abae340c6496c7dd098c6b7db76978
emagdnimMQT <mark.w.novak@gmail.com>
2020-08-21 16:52:30 -0400

    update to homepage.js homepage.js

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -6,17 +6,15 @@ import { Link } from 'react-router-dom';
 
 function HomePage(props) {
     return(
-        <div className="homePage">
+        <div className="HomePage">
             <div>
                 <Hero title={props.title} subTitle={props.subTitle} text={props.text} />
             </div>
-            <div className="ButtonContainers">
-                <div className="HomePage-Buttons">
-                    <Link className="HomePage-Button-Link" to="/about">About</Link>
-                    <Link className="HomePage-Button-Link" to="/publications">Publications</Link>
-                    <Link className="HomePage-Button-Link" to="/editorials">Editorial Work</Link>
-                    <Link className="HomePage-Button-Link" to="/contact">Contact</Link>
-                </div>
+            <div className="ButtonContainer">
+                <Link className="HomePage-Button-Link" to="/about">About</Link>
+                <Link className="HomePage-Button-Link" to="/publications">Publications</Link>
+                <Link className="HomePage-Button-Link" to="/editorials">Editorial Work</Link>
+                <Link className="HomePage-Button-Link" to="/contact">Contact</Link>
             </div>
         </div>
     );
