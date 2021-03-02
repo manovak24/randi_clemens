56aef06059e33a1ca29c48e854fc238817399eac
emagdnimMQT <mark.w.novak@gmail.com>
2021-03-02 12:45:49 -0500

    Updates

---
+++ precache-manifest.05fcaa897217c389ba8e63053ec4ca07.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e2438aeab027f9b206da7aada82163",
+    "url": "/randi_clemens/index.html"
+  },
+  {
+    "revision": "43bedba91c6c6558a319",
+    "url": "/randi_clemens/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7c78697195794a5efce5",
+    "url": "/randi_clemens/static/css/main.2033e93f.chunk.css"
+  },
+  {
+    "revision": "43bedba91c6c6558a319",
+    "url": "/randi_clemens/static/js/2.2118bea2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/randi_clemens/static/js/2.2118bea2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c78697195794a5efce5",
+    "url": "/randi_clemens/static/js/main.de0aac57.chunk.js"
+  },
+  {
+    "revision": "01c5a59969a1e7634183",
+    "url": "/randi_clemens/static/js/runtime-main.da8b3455.js"
+  },
+  {
+    "revision": "f986039554ca8c6cd50ec123a8a690da",
+    "url": "/randi_clemens/static/media/48F39987-7E06-4896-BF77-7F0FB7C4295A_1_105_c.f9860395.jpeg"
+  },
+  {
+    "revision": "d77a732f39ea129e78b4621c6c000e87",
+    "url": "/randi_clemens/static/media/randi.d77a732f.jpg"
+  }
+]);
