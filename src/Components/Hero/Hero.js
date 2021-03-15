b7e6421c215539bc44eb8221c84fd2c4afc544fe
emagdnimMQT <mark.w.novak@gmail.com>
2021-03-15 14:11:47 -0400

    Added media queries for hero responsive styling

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -7,8 +7,8 @@ function Hero(props) {
     return (
         <Jumbotron className="jumbotron-container bg-transparent">
             <Container className="jumbotron-elements">
-                { props.title && <h1 style={{ fontSize: '3.5rem', fontWeight: '700' }}>{props.title}</h1> }
-                { props.subTitle && <h2 style={{ fontWeight:'700' }}>{props.subTitle}</h2> }
+                { props.title && <h1>{props.title}</h1> }
+                { props.subTitle && <h2>{props.subTitle}</h2> }
             </Container>
         </Jumbotron>
     );
