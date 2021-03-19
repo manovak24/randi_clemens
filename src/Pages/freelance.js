239bee3a9817073da56e7c643f2c7496fa8f7294
emagdnimMQT <mark.w.novak@gmail.com>
2021-03-19 08:58:49 -0400

    added content to freelance page.

--- src/Pages/freelance.js
+++ src/Pages/freelance.js
@@ -8,8 +8,22 @@ class Freelance extends React.Component {
                 <div>
                     <Hero title={this.props.title} />
                 </div>
-                <div>
-                    <h1>Hello World</h1>
+                <div className="freelance-container">
+                    <div className="consultation-container">
+                        <h1>Creative Consultation</h1>
+                        <p>Workshop style critique and feedback of creative writing</p>
+                    </div>
+                    <div className="education-container">
+                        <h1>Education</h1>
+                        <p>Creative workshops, composition and essay tutoring, proofreading</p>
+                    </div>
+                    <div className="marketing-social-container">
+                        <h1>Marketing and Social media</h1>
+                        <p>Copywriting, content creation including blog and social media posts, branding, and SEO</p>
+                    </div>
+                    <div className="inquiry-container">
+                        <p>Please contact for inquires and rates</p>
+                    </div>
                 </div>
             </div>
             
