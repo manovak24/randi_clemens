960c9c11ddb5663bed244b9db6f60f0edeb58b70
emagdnimMQT <mark.w.novak@gmail.com>
2021-03-20 12:23:01 -0400

    updated editorals page. Most likely going back and creating component and passing in props instead of hardcoding to make scalabe.

--- src/Pages/EditorialsPage.js
+++ src/Pages/EditorialsPage.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import Hero from '../Components/Hero/Hero';
-import Content from '../Components/Content/Content';
 
 function EditorialsPage(props) {
     return(
         <div>
-            <Hero title={props.title} />
-            <Content>
-                    <p>Add</p>
-                    <p>Publications</p>
-                    <p>List</p>
-            </Content>
+            <div>
+                <Hero title={props.title} />
+            </div>
+            
+            <div className="passages-container">
+                <h1>Passages North</h1>
+                <ul>
+                    <li>Managing Editor: Issue 42 (forthcoming spring 2021)</li>
+                    <li>Associate Editor: 
+                        <a href="https://www.passagesnorth.com/issue-40" target="_blank" rel="noopener noreferrer">  Issue 40,</a>
+                        <a href="https://www.passagesnorth.com/issue-39" target="_blank" rel="noopener noreferrer"> Issue 39</a>
+                    </li>
+                    <li><a href="https://www.passagesnorth.com/passagesnorthcom/2019/12/6/a-woman-possessed-by-becky-robison" target="_blank" rel="noopener noreferrer">Introduction for “A Woman Possessed”</a></li>
+                </ul>
+            </div>
+
+            <div className="ninth-container">
+                <h1>Ninth Letter</h1>
+                <ul>
+                    
+                </ul>
+            </div>
+                    
         </div> 
     )
 }
