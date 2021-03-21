7528058fed488b32709d5bcde0f9c33a98f47018
emagdnimMQT <mark.w.novak@gmail.com>
2021-03-21 12:10:55 -0400

    Added content to editorial page and worked on responsiveness.

--- src/Components/EditorialList/EditorialList.js
+++ src/Components/EditorialList/EditorialList.js
@@ -1,11 +1,101 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Col } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
 
 import '../EditorialList/EditorialList.css';
 
 class EditorialList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            editorials: [
+                {
+                    id: 1,
+                    title: 'Introduction for "A Woman Possessed"',
+                    link: 'https://www.passagesnorth.com/passagesnorthcom/2019/12/6/a-woman-possessed-by-becky-robison'
+                },
+                {
+                    id: 2,
+                    title: 'Introduction for “Without the Sun We Wouldn’t Know Love”',
+                    link: 'https://www.passagesnorth.com/passagesnorthcom/2020/1/10/without-the-sun-we-wouldnt-know-love-by-felicity-fenton'
+                },
+                {
+                    id: 3,
+                    title: 'Introduction for “Three Poems”',
+                    link: 'https://www.passagesnorth.com/passagesnorthcom/2018/12/11/three-poems-by-anastasia-stelse'
+                },
+                {
+                    id: 4,
+                    title: 'Introduction for “I Forgot the Eggs”',
+                    link: 'https://www.passagesnorth.com/passagesnorthcom/2020/6/12/i-forgot-the-eggs-by-arji-manuelpillai'
+                },
+                {
+                    id: 5,
+                    title: 'Introduction for “The Lost Boy”',
+                    link: 'https://www.passagesnorth.com/passagesnorthcom/2019/7/12/the-lost-boy-by-kathryn-merwin'
+                },
+                {
+                    id: 6,
+                    title: 'Introduction for “ldr blues beginning and ending with takis”',
+                    link: 'https://www.passagesnorth.com/passagesnorthcom/2020/6/26/ldr-blues-beginning-and-ending-with-takis-by-alejandro-perez'
+                },
+                {
+                    id: 7,
+                    title: 'Introduction to “Bell Pepper and Beef Blues”',
+                    link: 'https://www.passagesnorth.com/passagesnorthcom/bell-peppers-and-beef'
+                }
+            ],
+            passages: [
+                {
+                    id: 1,
+                    title: 'Issue 40',
+                    link: 'https://www.passagesnorth.com/issue-40'
+                },
+                {
+                    id: 2,
+                    title: 'Issue 39',
+                    link: 'https://www.passagesnorth.com/issue-39'
+                }
+            ]
+        }
+    }
+
     render() {
         return (
-
+            <Row> 
+                <Col  className="editorial-container">
+                    <div className="passages-container">
+                        <div className="passages-info-container">
+                            <h1>Passages North</h1>
+                            <p>Managing Editor: Issue 42 forthcoming spring 2021</p>
+                            <p>Associate Editor: 
+                                <a href="https://www.passagesnorth.com/issue-40" target="_blank" rel="noopener noreferrer">Issue 40</a>, 
+                                <a href="https://www.passagesnorth.com/issue-39" target="_blank" rel="noopener noreferrer">Issue 39</a>
+                            </p>
+                            <ul className="edit-container left-rule">
+                                {this.state.editorials.map(editorial => (
+                                    <li key={editorial.id} className="edit-list-container">   
+                                        <a href={editorial.link} target="_blank" rel="noopener noreferrer">{editorial.title}</a>                     
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                
+                    <div className="ninth-container">
+                        <div className="ninth-info-container">
+                            <h1>Ninth Letter</h1>
+                            <p>Editorial Intern:
+                                <a href="http://ninthletter.com/journal/147-13-2-fall-winter-2016" target="_blank" rel="noopener noreferrer">Vol. 13 No. 2</a>,
+                                <a href="http://www.ninthletter.com/journal/134-13-1-spring-summer-2016" target="_blank" rel="noopener noreferrer">Vol. 13 No. 1</a>
+                            </p>
+                            <p>Editorial Assistant:
+                                <a href="http://ninthletter.com/winter-2016/winter-16-main" target="_blank" rel="noopener noreferrer">Winter 2016</a>
+                            </p>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
         )
     }
 }
--- src/Pages/EditorialsPage.js
+++ src/Pages/EditorialsPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Hero from '../Components/Hero/Hero';
+import EditorialList from '../Components/EditorialList/EditorialList';
 
 function EditorialsPage(props) {
     return(
@@ -8,25 +9,8 @@ function EditorialsPage(props) {
                 <Hero title={props.title} />
             </div>
             
-            <div className="passages-container">
-                <h1>Passages North</h1>
-                <ul>
-                    <li>Managing Editor: Issue 42 (forthcoming spring 2021)</li>
-                    <li>Associate Editor: 
-                        <a href="https://www.passagesnorth.com/issue-40" target="_blank" rel="noopener noreferrer">  Issue 40,</a>
-                        <a href="https://www.passagesnorth.com/issue-39" target="_blank" rel="noopener noreferrer"> Issue 39</a>
-                    </li>
-                    <li><a href="https://www.passagesnorth.com/passagesnorthcom/2019/12/6/a-woman-possessed-by-becky-robison" target="_blank" rel="noopener noreferrer">Introduction for “A Woman Possessed”</a></li>
-                </ul>
-            </div>
-
-            <div className="ninth-container">
-                <h1>Ninth Letter</h1>
-                <ul>
-                    
-                </ul>
-            </div>
-                    
+            <EditorialList />
+            
         </div> 
     )
 }
