91f4c4fd86a1997921f9fed8ffcdd663d254d448
emagdnimMQT <mark.w.novak@gmail.com>
2021-05-27 11:43:56 -0400

    Refractored about page.

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Row, Col, Image } from 'react-bootstrap';
 
 import Hero from '../Components/Hero/Hero';
 import './AboutPage.css';
@@ -11,33 +10,30 @@ function AboutPage(props) {
     const randi = require('../Assets/randi.jpg');
 
     return(
-        <div>
+        <div className="about-container">
             <div>
                 <Hero title={props.title} />
             </div>
-            <div className="about-container">    
-                <Row>
-                    <Col xs={12} md={4} className="image-container">
-                        <Image src={randi} roundedCircle />
-                    </Col>
-                    <Col xs={12} md={8} className="bio-container">
-                        <div>
-                            <p>Randi Clemens is a poet, editor, and educator who lives in the Upper Peninsula of 
-                                Michigan. She received her BA in Creative Writing from the University of Illinois at
-                                Urbana-Champaign and her MFA in poetry from Northern Michigan University where
-                                she also taught composition and creative writing. Her poetry has been published in
-                                various online journals and has been nominated for the Best of the Net. She has worked
-                                as an editor for Ninth Letter and served as the Managing Editor of Passages North. She is
-                                interested in contemporary poetry, science writing, museum studies, feminist literary
-                                theory, women and gender studies, visual poetics, and social justice pedagogy. She is
-                                actively seeking a home for her full-length manuscript and freelance opportunities as a
-                                creative consultant and/or editor.</p>
-                        </div>
-                        <div className="button-resume-container">
-                            <Link className="button-resume-link" to={Resume} target="_blank" download>Download Resume</Link> 
-                        </div>   
-                    </Col>
-                </Row> 
+            <div>
+
+            </div>
+            <div>
+                <article className="bio-container">
+                    <img className="randi-image" src={randi} alt="professional"></img>
+                    <p>Randi Clemens is a poet, editor, and educator who lives in the Upper Peninsula of 
+                        Michigan. She received her BA in Creative Writing from the University of Illinois at
+                        Urbana-Champaign and her MFA in poetry from Northern Michigan University where
+                        she also taught composition and creative writing. Her poetry has been published in
+                        various online journals and has been nominated for the Best of the Net. She has worked
+                        as an editor for Ninth Letter and served as the Managing Editor of Passages North. She is
+                        interested in contemporary poetry, science writing, museum studies, feminist literary
+                        theory, women and gender studies, visual poetics, and social justice pedagogy. She is
+                        actively seeking a home for her full-length manuscript and freelance opportunities as a
+                        creative consultant and/or editor.</p>
+                </article>
+                <div className="button-container">
+                    <Link className="button-link" to={Resume} target="_blank" download>Download Resume</Link> 
+                </div>
             </div>
         </div>
     )
